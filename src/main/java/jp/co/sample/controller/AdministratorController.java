4f7f2b0f62886371cca4afb14c8fdb111bd49b64
saneh1sa <yuta.sanehisa@rakus.co.jp>
2021-05-21 16:12:28 +0900

    ログイン画面表示第5章
    ログイン時に使用するフォームの作成
    
    ログイン画面に表示する処理
    
    ログイン画面を表示する画面とコントローラー修正

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 /**
@@ -26,13 +27,32 @@ public class AdministratorController {
 	/**
 	 * InsertAdministratorForm(管理者情報登録フォーム)をインスタンス化している.
 	 * 
-	 * @return　インスタンス化されたInsertAdministratorFormを返す
+	 * @return　インスタンス化されたInsertAdministratorFormを返す.
 	 */
 	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
-		InsertAdministratorForm insertAdministratorForm = new InsertAdministratorForm();
-		return insertAdministratorForm;
+		return new InsertAdministratorForm();
 	};
+	
+	/**
+	 * LoginForm(ログイン時フォーム)をインスタンス化している.
+	 * 
+	 * @return インスタンス化されたLoginFormを返す.
+	 */
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		return new LoginForm();
+	}
+	
+	/**
+	 * ログイン画面に移行する.
+	 * 
+	 * @return　administrator/login ログイン画面にフォワード.
+	 */
+	@RequestMapping("/")
+	public String toLogin() {
+		return "administrator/login";
+	}
 	
 	/**
 	 * 管理者登録画面へフォワードしている.
@@ -61,4 +81,6 @@ public class AdministratorController {
 		
 		return "redirect:/";
 	}
+	
+	
 }
